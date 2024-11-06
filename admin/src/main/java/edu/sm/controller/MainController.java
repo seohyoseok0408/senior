b5ce2ed75fbdd79da937cee191be9d0a36871242
daniel <pejqueen7@gmail.com>
2024-11-06 19:16:04 +0900

    init admin tamplate

---
+++ admin/src/main/java/edu/sm/controller/MainController.java
@@ -0,0 +1,22 @@
+package edu.sm.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        model.addAttribute("header", "header");
+        model.addAttribute("center", "center");
+        model.addAttribute("sidebar", "sidebar");
+        return "index";
+    }
+
+}
---
+++ admin/src/main/webapp/views/center.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!-- JSTL -->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+
+<div class="content-body">
+    <h1>
+        test
+    </h1>
+</div>
