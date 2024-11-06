43fc5614f49ed6983c115e8093d971e5d275960a
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-11-06 15:10:28 +0900

    add: Admin.java

---
+++ admin/src/main/java/edu/sm/model/Admin.java
@@ -0,0 +1,17 @@
+package edu.sm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    private String adminId;
+    private String adminUsername;
+    private String adminPassword;
+    private String adminName;
+}
