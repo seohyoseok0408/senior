809508a80f636be9da43ed5e48c1fb579113a66f
daniel <pejqueen7@gmail.com>
2024-11-07 11:21:55 +0900

    feat: 메인 페이지 세션 기반 접근 제어 추가 및 로그인 리다이렉트 처리

--- admin/src/main/java/edu/sm/controller/MainController.java
+++ admin/src/main/java/edu/sm/controller/MainController.java
@@ -1,18 +1,24 @@
 package edu.sm.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MainController {
 
     @RequestMapping("/")
-    public String main(Model model) {
+    public String main(Model model, HttpSession session) {
+        // 로그인 하지 않은 상태로 접근시 로그인 반환
+        if (session.getAttribute("principal") == null) {
+            // If "principal" is null, redirect to the login page
+            return "redirect:/login";
+        }
+
         model.addAttribute("header", "header");
         model.addAttribute("center", "center");
         model.addAttribute("sidebar", "sidebar");
