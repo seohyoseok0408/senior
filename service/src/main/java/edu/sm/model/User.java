8ba4fc5645c65eed9a051e428ab16b1fa25c92fd
jin <jin14410@naver.com>
2024-11-07 20:59:07 +0900

    주소 암호화

--- service/src/main/java/edu/sm/model/User.java
+++ service/src/main/java/edu/sm/model/User.java
@@ -14,18 +14,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class User {
     private int userId;
-    private String username;
+    private String username;              // 사용자 아이디
     private String password;
     private String tel;
     private String email;
     private String name;
     private LocalDate birthday;
     private String zipcode;
-    private String detailAdd1;       // "user_detail_add1"와 매핑
-    private String detailAddr1;      // "user_detail_addr1"와 매핑
-    private String detailAddr2;      // "user_detail_addr2"와 매핑
-    private LocalDateTime regDate;
+    private String detailAdd1;            // 상세 주소 1
+    private String detailAddr1;           // 상세 주소 2
+    private String detailAddr2;           // 상세 주소 3
+    private LocalDateTime regDate;        // 회원가입 일자
     private String status;
     private String profileImage;
 }
-
