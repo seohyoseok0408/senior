372ccb09506e46cccff137c4673a555da1ce0b24
jin <jin14410@naver.com>
2024-11-08 14:53:26 +0900

    암호화 수정

--- service/src/main/java/edu/sm/config/SpringSecurity.java
+++ service/src/main/java/edu/sm/config/SpringSecurity.java
@@ -1,9 +1,9 @@
 package edu.sm.config;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,12 +17,14 @@ public class SpringSecurity {
     }
 
     @Bean
-    public StandardPBEStringEncryptor textEncoder(@Value("${app.key.algo}") String algo, @Value("${app.key.skey}") String skey) {
+    public StandardPBEStringEncryptor textEncoder(@Value("${app.key.algo}") String algo,
+                                                  @Value("${app.key.skey}") String skey) {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setAlgorithm(algo);
         encryptor.setPassword(skey);
         return encryptor;
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
