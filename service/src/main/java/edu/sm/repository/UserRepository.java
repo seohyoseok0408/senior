57137eb065d6212a8627b80be95af047c5e055fe
jin <jin14410@naver.com>
2024-11-08 17:01:05 +0900

    refactor: 롤백

--- service/src/main/java/edu/sm/repository/UserRepository.java
+++ service/src/main/java/edu/sm/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface UserRepository extends SMRepository<Integer, User> {
-    // SMRepository의 메서드들을 자동으로 상속 받습니다.
-    // 추가적인 메서드가 필요하면 여기에 선언할 수 있습니다.
+    User findByUsername(String username);
+    User findByTel(String tel);
+    User findByEmail(String email);
 }
