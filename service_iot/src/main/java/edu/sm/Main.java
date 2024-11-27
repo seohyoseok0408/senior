9285f0c361c0645048c7833d4274f5015160a1c8
dwlee <ehddn0510@naver.com>
2024-11-27 10:47:36 +0900

    feat: 시니어 건강정보 로그 저장 구현

---
+++ service_iot/src/main/java/edu/sm/Main.java
@@ -0,0 +1,20 @@
+package edu.sm;
+
+import edu.sm.util.HttpSendData;
+
+public class Main {
+	public static void main(String[] args) {
+		String url = "http://127.0.0.1/iot/senior";
+
+		for (int i = 0; i < 100; i++) {
+			// 랜덤 데이터 전송
+			HttpSendData.send(url);
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+}
